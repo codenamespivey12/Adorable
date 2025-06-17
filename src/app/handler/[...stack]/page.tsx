588e84e0975b39990057700cccae6cc199a7e130
codenamespivey12 <codenamespivey@sixtyoneeightyai.com>
2025-06-16 22:13:47 -0500

    r

--- src/app/handler/[...stack]/page.tsx
+++ src/app/handler/[...stack]/page.tsx
@@ -1,5 +1,5 @@
-import { stackServerApp } from "@/auth/stack-auth";
 import { StackHandler } from "@stackframe/stack";
+import { stackServerApp } from "@/auth/stack-auth";
 
 export default function Handler(props: unknown) {
   return <StackHandler fullPage app={stackServerApp} routeProps={props} />;
